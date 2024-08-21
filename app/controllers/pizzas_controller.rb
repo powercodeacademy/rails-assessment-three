2585780cfceddd34ae2b5ced25f0a02227b2014b
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-21 10:00:49 -0400

    Create new pizza with form

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,2 +1,33 @@
 class PizzasController < ApplicationController
+  def new
+    @pizzeria = Pizzeria.find(params[:pizzeria_id])
+    @pizza = @pizzeria.pizzas.build
+  end
+
+  def show
+    @pizza = Pizza.find(params[:id])
+  end
+
+  # def create
+  #   @pizza = Pizza.create(pizza_params)
+  #   render: @pizzeria
+  # end
+
+
+  def create
+    @pizzeria = Pizzeria.find(pizza_params[:pizzeria_id])
+    @pizza = @pizzeria.pizzas.build(pizza_params)
+
+    if @pizza.save
+      redirect_to @pizzeria, alert: 'Pizza was successfully created.'
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def pizza_params
+    params.require(:pizza).permit(:name,:description, :pizzeria_id)
+  end
 end
