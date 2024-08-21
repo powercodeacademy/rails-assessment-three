704c47ad614f02fd03b5bcdff0cbe15293974ec1
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-21 09:53:11 -0400

    Create nested resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  get '/pizzerias/new', to: 'pizzerias#new', as: 'new_pizzeria'
-  post '/pizzerias', to: 'pizzerias#create', as: 'pizzerias'
-  get '/pizzerias/:id', to: 'pizzerias#show', as: 'pizzeria'
-  get '/pizzerias', to: 'pizzerias#index'
+  resources :pizzas
+  # get '/pizzerias/new', to: 'pizzerias#new', as: 'new_pizzeria'
+  # post '/pizzerias', to: 'pizzerias#create', as: 'pizzerias'
+  # get '/pizzerias/:id', to: 'pizzerias#show', as: 'pizzeria'
+  # get '/pizzerias', to: 'pizzerias#index'
+  resources :pizzerias
+
+  resources :pizzerias do
+    resources :pizzas
+  end
 end
