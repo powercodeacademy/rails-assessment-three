a91a4ce75b5c5767068e5ed23cd7f9cc53c10f2b
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-26 15:14:53 -0400

    Create new pizza

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,11 +1,20 @@
 class PizzasController < ApplicationController
   def new 
+    @pizza = Pizza.new 
   end
 
   def create
+    @pizza = Pizza.create(pizza_params)
+    redirect_to @pizza
   end 
 
   def show 
     @pizza = Pizza.find(params[:id])
+  end
+
+  private 
+
+  def pizza_params 
+    params.require(:pizza).permit(:name, :address, :pizzeria_id)
   end
 end
