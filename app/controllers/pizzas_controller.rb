76be453cef9105abc1e97cd54499fbdd6a1d8a25
Nihar Patel <niharpatel52@gmail.com>
2025-08-26 11:49:49 -0400

    Create Pizza show, index, form, and new views

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,3 +1,35 @@
 class PizzasController < ApplicationController
-  belongs_to :pizzeria
+  before_action :set_pizza, only: [:show, :edit, :update]
+
+  def index
+    @pizzas = Pizza.all
+  end
+
+  def new
+    @pizza = Pizza.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @pizza.update(pizza_params)
+      redirect_to @pizza
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_pizza
+    @pizza = Pizza.find(params[:id])
+  end
+
+  def pizza_params
+    params.require(:pizza).permit(:name, :description, :pizzeria_id)
+  end
 end
