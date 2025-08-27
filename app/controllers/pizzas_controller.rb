c5aeb98b58140c949cb7c73d6d4b23b9aa13b6f4
Mike <mclancy96@gmail.com>
2025-08-27 11:57:32 -0400

    Create pizza show page and fix routing

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,21 +1,22 @@
 class PizzasController < ApplicationController
   def create
     @pizza = Pizza.new(pizza_params)
-    @pizzeria ||= Pizzeria.find(params[:pizzeria_id])
+    @pizzeria = Pizzeria.find(params[:pizzeria_id])
     if @pizza.save
-      redirect_to pizzeria_pizza_path(pizzeria_id: @pizzeria.id, pizza: @pizza)
+      redirect_to pizzeria_pizza_path(pizzeria_id: @pizzeria.id, id: @pizza.id)
     else
       render :new
     end
   end
 
   def show
-
+    @pizza = Pizza.find(params[:id])
+    @pizzeria = Pizzeria.find(params[:pizzeria_id])
   end
 
   def new
     @pizza = Pizza.new
-    @pizzeria ||= Pizzeria.find(params[:pizzeria_id])
+    @pizzeria = Pizzeria.find(params[:pizzeria_id])
   end
 
   private
