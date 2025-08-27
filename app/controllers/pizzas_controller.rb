3a51c768c2277e36250b7075a66eaa6c90beeafe
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-27 08:53:52 -0400

    Nest pizza within pizzeria

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,20 +1,30 @@
 class PizzasController < ApplicationController
+  before_action :set_pizzeria 
+
   def new 
-    @pizza = Pizza.new 
+    @pizza = @pizzeria.pizzas.build 
   end
 
   def create
-    @pizza = Pizza.create(pizza_params)
-    redirect_to @pizza
+    @pizza = @pizzeria.pizzas.build(pizza_params)
+    if @pizza.save 
+      redirect_to pizzeria_pizza_path(@pizzeria, @pizza)
+    else 
+      render :new 
+    end
   end 
 
   def show 
-    @pizza = Pizza.find(params[:id])
+    @pizza = @pizzeria.pizzas.find(params[:id])
   end
 
   private 
 
+  def set_pizzeria 
+    @pizzeria = Pizzeria.find(params[:pizzeria_id])
+  end
+
   def pizza_params 
-    params.require(:pizza).permit(:name, :address, :pizzeria_id)
+    params.require(:pizza).permit(:name, :address)
   end
 end
